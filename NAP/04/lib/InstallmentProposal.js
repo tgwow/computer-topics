ae51a7dbe830036e21eb6bf5667cd09d68a8ec79
Thiago Oliveira <thiago.2809@outlook.com.br>
2021-03-06 22:16:46 -0300

    feat: add failure test

--- NAP/04/lib/InstallmentProposal.js
+++ NAP/04/lib/InstallmentProposal.js
@@ -12,13 +12,11 @@ class InstallmentProposal {
 		}
 	}
 
-	generate (client) {
+	generate ({salary, lending}) {
 		switch (true) {
-			case client.salary < 1000.00:
-				this.proposal[0] = this.calculate(2, 2, client.lending);
-				this.proposal[1] = this.calculate(2, 3, client.lending);
-				break
-			case client.salary > 1000:
+			case salary < 1000.00:
+				this.proposal[0] = this.calculate(2, 2, lending);
+				this.proposal[1] = this.calculate(2, 3, lending);
 				break
 			default: throw 'Unhandled proposal for this salary'
 		}
