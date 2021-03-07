a54d1f2c3c8a6421ed09d13fc748a0f2c47113ae
Thiago Oliveira <thiago.2809@outlook.com.br>
2021-03-06 22:20:43 -0300

    feat: add passed test

--- NAP/04/lib/InstallmentProposal.js
+++ NAP/04/lib/InstallmentProposal.js
@@ -18,6 +18,11 @@ class InstallmentProposal {
 				this.proposal[0] = this.calculate(2, 2, lending);
 				this.proposal[1] = this.calculate(2, 3, lending);
 				break
+			case (salary > 1000.01 && salary < 5000.00):
+				this.proposal[0] = this.calculate(1.3, 2, lending);
+				this.proposal[1] = this.calculate(1.5, 4, lending);
+				this.proposal[2] = this.calculate(1.5, 10, lending);
+				break
 			default: throw 'Unhandled proposal for this salary'
 		}
 	}
